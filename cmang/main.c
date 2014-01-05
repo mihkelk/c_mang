4aa49e97d1125dd19da933fb5d7e84a9c9a470fb
Mihkel <mihkelkuslapuu@gmail.com>
2014-01-05 20:46:50 +0200

    Numbri '3' liigutamine ruudustikus kasutades 'W', 'A', 'S', 'D' klahve.

--- cmang/main.c
+++ cmang/main.c
@@ -6,37 +6,64 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
 	int x;
 	int y;
 	int koordinaadid[64][64];
 	int px = 2;
 	int py = 2;
+	int suurus = 20;
 
-	for (y = 0; y < 21; y++)
+	while ("TRUE")
 	{
-		for (x = 0; x < 21; x++)
+		for (y = 0; y < (suurus + 1); y++)
 		{
-			if (x == px && y == py)
+			for (x = 0; x < (suurus + 1); x++)
 			{
-				koordinaadid[y][x] = 3;
-				printf("%d", koordinaadid[y][x]);
-			}
-			else if ((y % 4 == 0) || (x % 4 == 0))
-			{
-				koordinaadid[y][x] = 1;
-				printf("%d", koordinaadid[y][x]);
-			}
-			else
-			{
-				koordinaadid[y][x] = 0;
-				printf("%d", koordinaadid[y][x]);
-			};
-		};
-		printf("\n");
-	};
+				if (x == px && y == py)
+				{
+					koordinaadid[y][x] = 3;
+					printf("%d", koordinaadid[y][x]);
+				}
+				else if ((y == suurus || y == 0) || (x == suurus || x == 0))
+				{
+					koordinaadid[y][x] = 1;
+					printf("%d", koordinaadid[y][x]);
+				}
+				else
+				{
+					koordinaadid[y][x] = 0;
+					printf("%d", koordinaadid[y][x]);
+				};
 
-	getch();
+			};
+
+			printf("\n");
+
+		};
+
+		char klahv = getch();
+		printf("%c", klahv);
+		if (klahv == 'd' && (koordinaadid[py][px + 1] != 1))
+		{
+			px++;
+		}
+		else if (klahv == 'a' && (koordinaadid[py][px - 1] != 1))
+		{
+			px--;
+		}
+		if (klahv == 'w' && (koordinaadid[py][py - 1] != 1))
+		{
+			py--;
+		}
+		if (klahv == 's' && (koordinaadid[py][py + 1] != 1))
+		{
+			py++;
+		}
+		system("cls"); // tühjendab ekraani
+	}
 	return 0;
 }
