69f9338566dfe6c905721d77b3ca2eb74c8dc4d6
Mihkel <mihkelkuslapuu@gmail.com>
2014-01-05 21:06:31 +0200

    PDCurses kasutuselevõtt.

--- cmang/main.c
+++ cmang/main.c
@@ -1,178 +1,139 @@
 /*
  * main.c
  *
- *  Created on: 23.10.2012
- *      Author: mihkelk
+ *  Created on: 02.11.2012
+ *      Author: Mihkel
  */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <windows.h> //värvide jaoks
-int x;
-int y;
-int z;
-int koordinaadid[100][100][100];
-int suurus = 1000;
-int m = 0;
-int px = 30;
-int py = 30;
-int jsuurus = 10; //joonistatava vaatevälja suurus
+#include <windows.h>
+#include <curses.h>
+
+int x, y, z, koordinaadid[100][100][100];
+int px = 30, py = 30; //mängija algkoordinaadid
+int jsuurus = 25; //joonistatava vaatevälja suurus
+BOOL WINAPI SetConsoleFont(HANDLE hOutput, DWORD fontIndex);
 
 // Maailma loomine failist
 int maailm()
 {
-	//int x;
-	//int y;
-//char failinimi[7];
- //char failinimi = "z"+ znumber + ".txt"
-// korrus b1
-	FILE *bp = fopen("z49.txt", "r"); //Faili sisselugemine
-	char asukohtb1 = '0';
+	char cz[10];
 
-	z = 49;
-
-	for (y = 0; asukohtb1 != '-'; y++)
+	for (z = 49; z < 51; z++)
 	{
-		for (x = 0; asukohtb1 != '\n'; x++)
+
+		cz[10] = itoa(z, cz, 10); //itoa(arv mida teisaldan, string millesse teisaldan, arvusüsteem)
+		//	printf("%s", cz); //[0], cz[1], cz[2]);
+
+		FILE *korrus = fopen(cz, "r");
+		char casukoht = '0';
+		for (y = 0; casukoht != '-'; y++)
 		{
-			asukohtb1 = fgetc(bp);
-			switch (asukohtb1)
+			for (x = 0; casukoht != '\n'; x++)
 			{
-			case '0':
-				koordinaadid[z][y][x] = 0; //tühjus
-				break;
-			case '1':
-				koordinaadid[z][y][x] = 1; //sein
-				break;
-			case '2':
-				koordinaadid[z][y][x] = 2; //muru
-				break;
-			case '4':
-				koordinaadid[z][y][x] = 4; //põõsas
-				break;
-			case '5':
-				koordinaadid[z][y][x] = 5; //trepp alla
-				break;
-			case '6':
-				koordinaadid[z][y][x] = 6; //trepp yles
-				break;
-			case '7':
-				koordinaadid[z][y][x] = 7; //puitpõrand
-				break;
-			default:
-				koordinaadid[z][y][x] = 9;
-				break;
-			}
+				casukoht = fgetc(korrus);
+
+				switch (casukoht)
+				{
+				case '0':
+					koordinaadid[z][y][x] = 0; //tühjus
+					break;
+				case '1':
+					koordinaadid[z][y][x] = 1; //sein
+					break;
+				case '2':
+					koordinaadid[z][y][x] = 2; //muru
+					break;
+				case '4':
+					koordinaadid[z][y][x] = 4; //põõsas
+					break;
+				case '5':
+					koordinaadid[z][y][x] = 5; //trepp alla
+					break;
+				case '6':
+					koordinaadid[z][y][x] = 6; //trepp yles
+					break;
+				case '7':
+					koordinaadid[z][y][x] = 7; //puitpõrand
+					break;
+				default:
+					koordinaadid[z][y][x] = 9;
+					break;
+				}
+			};
+			casukoht = fgetc(korrus);
 		};
-		asukohtb1 = fgetc(bp);
-	};
 
-	printf("Laetud 50");
-	// korrus 0
-
-	FILE *fp = fopen("z50.txt", "r"); //Faili sisselugemine
-	char asukoht = '0';
-	z = 50;
-
-	for (y = 0; asukoht != '-'; y++)
-	{
-		for (x = 0; asukoht != '\n'; x++)
-		{
-			asukoht = fgetc(fp);
-			switch (asukoht)
-			{
-			case '0':
-				koordinaadid[z][y][x] = 0; //tühjus
-				break;
-			case '1':
-				koordinaadid[z][y][x] = 1; //sein
-				break;
-			case '2':
-				koordinaadid[z][y][x] = 2; //muru
-				break;
-			case '4':
-				koordinaadid[z][y][x] = 4; //põõsas
-				break;
-			case '5':
-				koordinaadid[z][y][x] = 5; //trepp alla
-				break;
-			case '6':
-				koordinaadid[z][y][x] = 6; //trepp yles
-				break;
-			default:
-				koordinaadid[z][y][x] = 9;
-				break;
-			}
-		};
-		asukoht = fgetc(fp);
-	};
+	}
 
 	return 1;
 }
 
+//http://blogs.microsoft.co.il/blogs/pavely/archive/2009/07/23/changing-console-fonts.aspx
+
 //-------------------------------joonistamine-----------------------------//
 int joonistamine()
 {
-	system("cls");
 
-	for (y = py - jsuurus; y < (py + jsuurus + 1); y++)
+	//raw();
+	curs_set(0); //peidab kursori
+
+	int reavahetus = -1;
+
+	for (y = py - jsuurus; y < (py + jsuurus); y++)
 	{
-		for (x = px - jsuurus; x < (px + jsuurus + 1); x++)
+		reavahetus++;
+		move(reavahetus, 0);
+		for (x = px - jsuurus; x < (px + jsuurus); x++)
 		{
-			if (x == px && y == py)
-			{
+
+			if (x == px && y == py) {
 				koordinaadid[z][y][x] = 3;
 			}
 			switch (koordinaadid[z][y][x])
 			{
 			case 0:
-				//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 34);
-				printf(" ");
+				addch(ACS_BLOCK | COLOR_PAIR(6)); // sümbolid laiendatud ascii tabelist pdcursesiga nimetused ACS_*
 				break;
 			case 1:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-				printf("\333"); //ASCII koodid kaheksanddsüsteemis
+				addch(ACS_BLOCK | COLOR_PAIR(1)); // Värve võetakse paaridena mis on defineeritud peafunktsioonis.
 				break;
 			case 2:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
-				printf("\261");
+				addch(ACS_CKBOARD | COLOR_PAIR(2));
 				break;
 			case 3:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 154);
-				printf("\1");
+				addch('\1' | COLOR_PAIR(4) | A_ALTCHARSET); // altcharset vajalik naerunäeo näitamiseks
 				break;
 			case 4:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 294); //42
-				printf("\260");
+				addch(ACS_BOARD | COLOR_PAIR(5));
 				break;
 			case 5:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-				printf("v");
+				addch('v' | COLOR_PAIR(4));
 				break;
 			case 6:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-				printf("^");
+				addch('^' | COLOR_PAIR(4));
 				break;
 			case 7:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2680);
-				printf("\262");
-				// \334
+				addch(ACS_CKBOARD | COLOR_PAIR(7));
+				break;
+			default:
+				printw("%d", koordinaadid[z][y][x]);
 				break;
 
-			default:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-				printf("%d", koordinaadid[z][y][x]);
-				break;
 			};
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0); // värv tagasi mustaks, et ei tomuks jama taustavärvidega
 
 		};
-		printf("\n");
+
+
 	};
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-	printf("Kulda 50");
-	printf("   Elusid 50\n");
-	printf("Kõike 50");
+
+
+	//printw("Kulda 50");
+	//printw("   Elusid 50\n");
+	//printw("Kõike 50");
+	SetConsoleFont(GetStdHandle(STD_OUTPUT_HANDLE), 2); // vahetab fondi suurust, vajalik kuna kaotab ära jooned suuremat vilkumist põhjustamatta
+	//move(0, 0);
+
 	return 0;
 }
 //------------------------------------------------------------------------//
@@ -182,20 +143,43 @@ int main()
 {
 
 	maailm();
+
+	initscr();
+	raw();
+	noecho();
+
+//http://pdcurses.sourceforge.net/doc/PDCurses.txt
+
+	resize_term(50, 50); //konsooli suuruse muutmine
+
+	//PDCurses vervindus
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_WHITE);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(4, COLOR_RED, COLOR_WHITE);
+	init_pair(5, COLOR_GREEN, COLOR_YELLOW);
+	init_pair(6, COLOR_BLACK, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+
 	z = 50;
 	joonistamine();
 
-	int Q = 1;
+	BOOL lopp = FALSE;
 	int rs1 = 0; // salvestab ruudu kus mängja seisab, et see hiljem taastada
 	int rs2 = 0; // salvestab teise ruudu kus mängija seisis
 	int s1 = 1; // samm 1
 	int s2 = 0; // samm 2
-	while (Q == 1) {
+
+	while (lopp == FALSE)
+	{
 //----------Klahvivajutuste-järgi-mängija-koordinaatide-muutmine----------//
+
 		char klahv = getch();
 
 		if (klahv == 'd' && (koordinaadid[z][py][px + 1] != 1))
 		{
+
 			if (s1 == 1)
 			{
 				s2 = 0;
@@ -212,6 +196,7 @@ int main()
 				s2 = 0;
 				s1 = 1;
 			}
+
 		}
 		else if (klahv == 'a' && (koordinaadid[z][py][px - 1] != 1))
 		{
@@ -256,6 +241,7 @@ int main()
 		}
 		if (klahv == 's' && (koordinaadid[z][py + 1][px] != 1))
 		{
+
 			if (s1 == 1)
 			{
 				s2 = 0;
@@ -272,10 +258,11 @@ int main()
 				s2 = 0;
 				s1 = 1;
 			}
+
 		}
 		if (klahv == 'q')
 		{
-			Q = 0;
+			lopp = TRUE;
 		}
 		s2 = 1;
 
@@ -293,14 +280,15 @@ int main()
 
 			break;
 		}
-		joonistamine();
-		Sleep(10);
 
+		refresh();          //////////////////////
+		joonistamine();  	//peab esinema mitu	//
+		refresh();			//korda et vältida	//
+		joonistamine();		//viga värvidega	//
+							//////////////////////
 	};
 	return 0;
 //------------------------------------------------------------------------//
 }
 //------------------------------------------------------------------------//
-
-// SetCursorPos(xpos,ypos); kursori paigutamine kuhugi, pole veel kasutusel
 
