9622b2efe4ec50a889a4ee01de8f6db4ab8b2798
Mihkel <mihkelkuslapuu@gmail.com>
2014-01-05 21:16:15 +0200

    Möödunud sammude väärtusi ei talletata enam.

--- cmang/main.c
+++ cmang/main.c
@@ -1,14 +1,13 @@
 /*
- * m8.c
+ * main.c
  *
- *  Created on: 07.11.2012
+ *  Created on: 07.06.2013
  *      Author: mihkelk
  */
 
 #include <stdlib.h>
 #include <windows.h> //Vajalik fondi muutuste käskluse jaoks.
 #include <curses.h>  //Vajalik PDCurses kasutamiseks.
-
 int x, y, z;
 char koordinaadid[100][500][500]; //Toetab 100 korrust mille suuruseks umbes 500x500 tähemärki, reavahetus loeb samuti tähemärgina.
 int px = 248, py = 247; //mängija algkoordinaadid
@@ -62,40 +61,44 @@ int joonistamine()
 		for (x = px - jsuurus; x < (px + jsuurus); x++)
 		{
 
-			if (x == px && y == py)
-			{
-				koordinaadid[z][y][x] = '3';
-			}
-			switch (koordinaadid[z][y][x])
-			{
-			case '0': //Tühjus
-				addch(ACS_BLOCK | COLOR_PAIR(6)); // Sümbolid laiendatud ascii tabelist pdcursesiga nimetused ACS_*
-				break;
-			case '1': //Sein
-				addch(ACS_BLOCK | COLOR_PAIR(1)); // Värve võetakse paaridena mis on defineeritud peafunktsioonis.
-				break;
-			case '2': //Muru
-				addch(ACS_CKBOARD | COLOR_PAIR(2));
-				break;
-			case '3': //Mängija
+			if (x == px && y == py) {
 				addch('\1' | COLOR_PAIR(4) | A_ALTCHARSET); // Altcharset vajalik naerunäeo näitamiseks.
-				break;
-			case '4': //Heledam muru
-				addch(ACS_BOARD | COLOR_PAIR(5));
-				break;
-			case '5': //Trepp alla
-				addch('v' | COLOR_PAIR(4));
-				break;
-			case '6': //Trepp üles
-				addch('^' | COLOR_PAIR(4));
-				break;
-			case '7': //Kivipõrand
-				addch(ACS_CKBOARD | COLOR_PAIR(7));
-				break;
-			default:
-				printw("%d", koordinaadid[z][y][x]);
-				break;
 
+			}
+			else
+			{
+
+				switch (koordinaadid[z][y][x])
+				{
+				case '0': //Tühjus
+					addch(ACS_BLOCK | COLOR_PAIR(6)); // Sümbolid laiendatud ascii tabelist pdcursesiga nimetused ACS_*
+					break;
+				case '1': //Sein
+					addch(ACS_BLOCK | COLOR_PAIR(1)); // Värve võetakse paaridena mis on defineeritud peafunktsioonis.
+					break;
+				case '2': //Muru
+					addch(ACS_CKBOARD | COLOR_PAIR(2));
+					break;
+				case '3': //Mängija
+					addch('\1' | COLOR_PAIR(4) | A_ALTCHARSET); // Altcharset vajalik naerunäeo näitamiseks.
+					break;
+				case '4': //Heledam muru
+					addch(ACS_BOARD | COLOR_PAIR(5));
+					break;
+				case '5': //Trepp alla
+					addch('v' | COLOR_PAIR(4));
+					break;
+				case '6': //Trepp üles
+					addch('^' | COLOR_PAIR(4));
+					break;
+				case '7': //Kivipõrand
+					addch(ACS_CKBOARD | COLOR_PAIR(7));
+					break;
+				default:
+					printw("%d", koordinaadid[z][y][x]);
+					break;
+
+				};
 			};
 
 		};
@@ -118,8 +121,10 @@ int main()
 
 //http://pdcurses.sourceforge.net/doc/PDCurses.txt
 
-	resize_term(jsuurus * 2, jsuurus * 2); //Konsooli suuruse muutmine.
-
+	//resize_term(jsuurus * 2, jsuurus * 2); //Konsooli suuruse muutmine.
+	resize_term(jsuurus * 2 + 60, jsuurus * 2 + 60); //Konsooli suuruse muutmine.
+	move(55,0);
+	printf("joonistamine");
 	//PDCurses värvindus.
 	start_color(); //Alustab värvide kasutust.
 	//---------Värvipaaride määramine.---------//
@@ -135,9 +140,6 @@ int main()
 	joonistamine();
 
 	BOOL lopp = FALSE; //
-	char rs1 = '0'; // Salvestab ruudu kus mängja seisab, et see hiljem taastada.
-	char rs2 = '2'; // Salvestab teise ruudu kus mängija seisis.
-	int s1 = 1, s2 = 0; // Sammuloendurid 1 ja 2.
 
 	while (lopp == FALSE)
 	{
@@ -148,98 +150,36 @@ int main()
 
 		if (klahv == 'd' && (koordinaadid[z][py][px + 1] != '1'))
 		{
-			if (s1 == 1)
-			{
-				s2 = 0;
-				rs1 = koordinaadid[z][py][px + 1];
-				px++;
-				koordinaadid[z][py][px - 1] = rs2;
-				s1 = 0;
-			}
-			if (s2 == 1)
-			{
-				rs2 = koordinaadid[z][py][px + 1];
-				px++;
-				koordinaadid[z][py][px - 1] = rs1;
-				s2 = 0;
-				s1 = 1;
-			}
+			px++;
+
 		}
 		else if (klahv == 'a' && (koordinaadid[z][py][px - 1] != '1'))
 		{
-			if (s1 == 1)
-			{
-				s2 = 0;
-				rs1 = koordinaadid[z][py][px - 1];
-				px--;
-				koordinaadid[z][py][px + 1] = rs2;
-				s1 = 0;
-			}
-			if (s2 == 1)
-			{
-				rs2 = koordinaadid[z][py][px - 1];
-				px--;
-				koordinaadid[z][py][px + 1] = rs1;
-				s2 = 0;
-				s1 = 1;
-			}
+			px--;
+
 		}
 		else if (klahv == 'w' && (koordinaadid[z][py - 1][px] != '1'))
 		{
-			if (s1 == 1)
-			{
-				s2 = 0;
-				rs1 = koordinaadid[z][py - 1][px];
-				py--;
-				koordinaadid[z][py + 1][px] = rs2;
-				s1 = 0;
-			}
-			if (s2 == 1)
-			{
-				rs2 = koordinaadid[z][py - 1][px];
-				py--;
-				koordinaadid[z][py + 1][px] = rs1;
-				s2 = 0;
-				s1 = 1;
-			}
+			py--;
+
 		}
 		else if (klahv == 's' && (koordinaadid[z][py + 1][px] != '1'))
 		{
-			if (s1 == 1)
-			{
-				s2 = 0;
-				rs1 = koordinaadid[z][py + 1][px];
-				py++;
-				koordinaadid[z][py - 1][px] = rs2;
-				s1 = 0;
-			}
-			if (s2 == 1)
-			{
-				rs2 = koordinaadid[z][py + 1][px];
-				py++;
-				koordinaadid[z][py - 1][px] = rs1;
-				s2 = 0;
-				s1 = 1;
-			}
+			py++;
+
 		}
 		else if (klahv == 'q')
 		{
 			lopp = TRUE;
 		}
 
-		s2 = 1;
-
-		switch (koordinaadid[z][py][px])	//Kui on tegu treppidega.
+		switch (koordinaadid[z][py][px]) //Kui on tegu treppidega.
 		{
 		case '5':
 			z--;
-			rs1 = '7';
-			rs2 = '7';
 			break;
 		case '6':
 			z++;
-			rs1 = '7';
-			rs2 = '7';
 			break;
 		}
 		//------------------------------------------------------------------------//
